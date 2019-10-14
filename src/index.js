568673cb48718954d1fd3eb25712bcf2ae1b4f3b
Andres Campero <andres.campero@frubana.com>
2019-10-14 16:48:57 -0500

    configuracion inicial de redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./routes/App";
+import { Provider } from "react-redux";
+import store from "./store";
 
-ReactDom.render(<App />, document.getElementById("app"));
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
